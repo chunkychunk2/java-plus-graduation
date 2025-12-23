185c233df67c3ed1e758373fdd6d405a2e4a99d8
chun <chunoff@list.ru>
2025-12-23 18:59:11 +0300

    move duplicate getPageable method

--- core/interaction-api/src/main/java/ru/practicum/parameters/PageableSearchParam.java
+++ core/interaction-api/src/main/java/ru/practicum/parameters/PageableSearchParam.java
@@ -1,21 +1,11 @@
 package ru.practicum.parameters;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import lombok.experimental.SuperBuilder;
 
 @AllArgsConstructor
-@Builder
+@SuperBuilder
 @Getter
-public class PageableSearchParam {
-
-    private Integer from;
-    private Integer size;
-
-    public Pageable getPageable() {
-        int page = from / size;
-        return PageRequest.of(page, size);
-    }
+public class PageableSearchParam extends PageableParam {
 }
