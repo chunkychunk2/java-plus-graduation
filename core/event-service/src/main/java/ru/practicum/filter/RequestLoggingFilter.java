1cd5327a37935b7c191d745bb64e0931becfa0b8
chun <chunoff@list.ru>
2025-12-24 17:00:18 +0300

    first review fixes

--- core/event-service/src/main/java/ru/practicum/filter/RequestLoggingFilter.java
+++ core/event-service/src/main/java/ru/practicum/filter/RequestLoggingFilter.java
@@ -6,50 +6,32 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.UUID;
+
 
 @Component
 @Slf4j
 public class RequestLoggingFilter implements Filter {
 
-    public static final String CORRELATION_ID_HEADER = "X-Correlation-Id";
-
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-
-        String correlationId = httpRequest.getHeader(CORRELATION_ID_HEADER);
-        if (correlationId == null || correlationId.isEmpty()) {
-            correlationId = UUID.randomUUID().toString();
-        }
-        MDC.put("correlationId", correlationId);
-
-        long startTime = System.currentTimeMillis();
 
         String method = httpRequest.getMethod();
         String uri = httpRequest.getRequestURI();
         String queryString = httpRequest.getQueryString();
         String remoteAddr = httpRequest.getRemoteAddr();
 
-        String logMessage = "Incoming Request: {} {}" + (queryString != null && !queryString.isEmpty() ? "?{}" : "") +
-                " from {}";
-        log.info(logMessage, method, uri, queryString, remoteAddr);
-
-        try {
-            chain.doFilter(request, response);
-        } finally {
-            long duration = System.currentTimeMillis() - startTime;
-            log.info("Outgoing Response: {} {} {} in {}ms (Status: {})", method, uri, (queryString != null &&
-                    !queryString.isEmpty() ? "?" + queryString : ""), remoteAddr, duration, httpResponse.getStatus());
-            MDC.remove("correlationId");
+        if (queryString != null && !queryString.isEmpty()) {
+            log.info("Incoming Request: {} {}?{} from {}", method, uri, queryString, remoteAddr);
+        } else {
+            log.info("Incoming Request: {} {} from {}", method, uri, remoteAddr);
         }
+
+        chain.doFilter(request, response);
     }
 }
